f40703de87e103279d607432f0fc3ecb389eab14
StephanyNM4 <stephanymatamoros4@gmail.com>
2024-11-10 03:34:43 -0600

    estructura inicial de obtener productos

---
+++ src/app/productos/components/Products.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function Products () {
+    return (
+        <h1 className='size-96 text-black'>.</h1>
+    )
+}
---
+++ src/app/productos/components/interacciones/Boton.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Boton () {
+    return (
+        <div >
+            <button className="w-[90%] size-8 py-1 px-3 bg-purple-500 hover:bg-purple-700 rounded-md shadow-lg text-white font-semibold transition duration-200"
+                    type="submit">
+                    <p className="">+ Añadir producto</p>
+            </button>
+        </div>
+        
+    )
+}
