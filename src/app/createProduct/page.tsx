a608ece59f28784f5214d5dfdcedf048cc2bb923
ale2002solano <ale134valle@gmail.com>
2024-11-22 09:39:38 -0600

    sidebar

--- src/app/createProduct/page.tsx
+++ src/app/createProduct/page.tsx
@@ -3,7 +3,7 @@
 import Footer from "components/Footer";
 import ProductForm from "./components/Product_notallas_Form";
 import HeaderAdmin from "components/HeaderAdmin";
-import SideBar from "components/Sidebar";
+import SideBar from "components/sidebar";
 
 
 export default function Dashboard() {
