74483b31a3a062bfbf4db8f373c4e16b47674ba2
ale2002solano <ale134valle@gmail.com>
2024-11-22 10:36:26 -0600

    side

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -2,7 +2,6 @@
 
 
 import HeaderAdmin from 'components/HeaderAdmin'
-import SideBar from "components/sidebar";
 import SideBar from 'components/sidebar'
 import Titulo from './components/Titulo'
 import Interactions from './components/Interactions'
