9de3116e0de89802de8d118be4028840ac941b33
ale2002solano <ale134valle@gmail.com>
2024-11-22 09:45:05 -0600

    sidebar

--- src/app/ordenes/page.tsx
+++ src/app/ordenes/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "components/sidebar";
 import Headerbar from "../../components/HeaderAdmin";
 
 export default function ShopCart() {
